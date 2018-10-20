e4217a13f367b609249117753c86940b62841083
Christoph <Christoph.Gutwein@gmx.de>
2018-10-20 18:58:33 +0200

    LandmarkDetector intial

--- app/src/main/java/de/gdgnuremberg/mobile/citytours/MainActivity.kt
+++ app/src/main/java/de/gdgnuremberg/mobile/citytours/MainActivity.kt
@@ -1,15 +1,24 @@
 package de.gdgnuremberg.mobile.citytours
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat.startActivityForResult
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmark
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -36,6 +45,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.let {
@@ -57,5 +67,44 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp() =
             Navigation.findNavController(this, R.id.mainNavigationFragment).navigateUp()
+
+    private fun detector(bitmap: Bitmap) {
+        val image = FirebaseVisionImage.fromBitmap(bitmap)
+
+        // Set the default settings
+        val detector = FirebaseVision.getInstance()
+                .visionCloudLandmarkDetector
+
+        val result = detector.detectInImage(image)
+                .addOnSuccessListener { firebaseVisionCloudLandmarks ->
+                    for (landmark in firebaseVisionCloudLandmarks) {
+
+                        val bounds = landmark.boundingBox
+                        val landmarkName = landmark.landmark
+                        val entityId = landmark.entityId
+                        val confidence = landmark.confidence
+                        Log.v("detector", "bounds: $landmarkName")
+                        Log.v("detector", "entityId: $entityId")
+
+                        // Multiple locations are possible, e.g., the location of the depicted
+                        // landmark and the location the picture was taken.
+                        for (loc in landmark.locations) {
+                            val latitude = loc.latitude
+                            val longitude = loc.longitude
+
+                            Log.v("detector", "latitude: " + java.lang.Double.toString(latitude))
+                            Log.v("detector", "longitude: " + java.lang.Double.toString(longitude))
+
+                        }
+                    }
+                }
+                .addOnFailureListener { e ->
+                    // Task failed with an exception
+                    Log.e("Landmark", e.message)
+                    Toast.makeText(this@LandmarkDetector,
+                            e.message, Toast.LENGTH_LONG).show()
+                }
+
+    }
 
 }
