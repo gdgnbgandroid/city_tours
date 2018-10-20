1e87e219604ee5d1684802094b55b27a4c47befb
DaemonLoki8 <stefan.blos@gmail.com>
2018-10-20 17:51:53 +0200

    added code to get image from camera

--- app/src/main/java/de/gdgnuremberg/mobile/citytours/MainActivity.kt
+++ app/src/main/java/de/gdgnuremberg/mobile/citytours/MainActivity.kt
@@ -1,12 +1,47 @@
 package de.gdgnuremberg.mobile.citytours
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mStartCameraButton = findViewById<Button>(R.id.button_take_image);
+        mStartCameraButton.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
     }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.let {
+                it.get("data") as Bitmap
+
+            }
+            if (imageBitmap != null) {
+                val mImageView = findViewById<ImageView>(R.id.iv_taken_image)
+                mImageView.setImageBitmap(imageBitmap)
+            }
+
+        }
+    }
+
 }
